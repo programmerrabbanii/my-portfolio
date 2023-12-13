dce6ca8e48c8e3ca44af9a850b53cff8a6ced5aa
programmerrabbanii <rabbanisarkar642@gmail.com>
2023-12-13 21:54:00 +0600

    added the icon

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,45 +1,48 @@
+import { Link } from 'react-router-dom';
 import logofooter from '../../assets/projectimg/alllogo.png'
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+
+
+
+
+
 const Footer = () => {
     return (
-        <div className='mainfooter text-white   bg-[#1F2937] grid grid-cols-1 lg:grid-cols-4 py-8 gap-6 '>
-      
-    <div className="mainfooter  ">
-    <div className="footerlogo">
-     <aside>
-    <img className='w-[170px] bg-[#1F2937]' src={logofooter} alt="" />
-    <p>Hello, and welcome to my website! My name is Md Jahid Hasan, and I am a web designer and developer with 3 years of experience in the field
-</p>
+        <div className='mainfooter text-white   bg-[#1F2937]'>
+            <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+  <aside className="items-center grid-flow-col">
+   <img className='w-[100px]' src={logofooter} alt="" />
+    <p>Copyright © 2021 - All right reserved Dev Rabbani Sarkar</p>
   </aside> 
-     </div>
-    </div> 
-   <div className="services-footer">
-   <nav>
+  <Link>
+  <FaFacebook/> 
   
-    <header className="footer-title">Services</header> 
-    <a className="link link-hover">Full website creation</a> <br />
-    <a className="link link-hover">E-commerce website</a> <br />
-    <a className="link link-hover">Business Website</a> <br />
-    <a className="link link-hover">Landing page</a> <br />
-    <a className="link link-hover">Website Maintenance</a> <br />
-  </nav> 
-   </div>
-    
-     <div className="footerses">
-     <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-     </div>
-     <div className="footerses">
-     <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </nav>
-     </div>
+  </Link>
+  <Link>
+  <FaLinkedin/>
+  
+  </Link>
+  <Link>
+  <FaGithub/>
+  
+  </Link>
+
+  <Link>
+  <FaInstagram/>
+  
+  </Link>
+
+  <Link>
+  <FaYoutube/>
+  
+  </Link>
+</footer>
+      
+   
 
 
             
